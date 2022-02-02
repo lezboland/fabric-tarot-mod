f4a067779ab235c332fd3e32d41185ed89c7edea
noah <noahtanooki@posteo.net>
2022-02-01 21:23:22 -0800

    Give block a model

---
+++ src/main/java/net/fabricmc/example/ExampleBlock.java
@@ -0,0 +1,9 @@
+package net.fabricmc.example;
+
+import net.minecraft.block.Block;
+
+public class ExampleBlock extends Block {
+    public ExampleBlock(Settings settings) {
+        super(settings);
+    }
+}
